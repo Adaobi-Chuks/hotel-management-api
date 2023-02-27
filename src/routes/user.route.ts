26118aaf8c62add0572f3c97a04ed9a0166e0502
Adaobi Chuks-Aginam <chuksaginamada@gmail.com>
2023-02-27 20:03:04 +0100

    update config file

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import UserController from '../controllers/user.controller';
-const {createUser, getUsers, getUserById, editUser, deleteUserById, login, logout} = new UserController();
+const {createUser, getUsers, getUserById, editUser, deleteUserById, login} = new UserController();
 import {validateUserReq, validateUserOpt} from "../middlewares/userValidation.middleware";
 import authenticate from "../middlewares/authentication.middleware";
 const router = express.Router();
@@ -18,6 +18,6 @@ router.delete("/:id", authenticate, deleteUserById);
 //login user
 router.post("/login", validateUserOpt, login);
 //logout user
-router.get("/logout", logout);
+// router.get("/logout", logout);
 
 export default router;
